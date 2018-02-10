8fab6dc0816b2515ea60bf3acebeafa64a9e836d
Ivan Rozhko <ivan.rozhko@gmail.com>
2018-02-10 14:02:00 +0200

    Basic routing layout

--- erp_angular/erp-app/src/app/app.module.ts
+++ erp_angular/erp-app/src/app/app.module.ts
@@ -1,20 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
+ 
+import { AppComponent }         from './app.component';
+import { DashboardComponent }   from './dashboard/dashboard.component';
+ 
+import { AppRoutingModule }     from './app-routing.module';
+ 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+  ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
